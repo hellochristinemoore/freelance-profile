de3f27368e39a399f9835e0a2057970517902d6c
hellochristinemoore <hello.christine.moore@gmail.com>
2021-05-12 21:46:00 +0200

    Corrected singular/multiple variable name issue inhibiting data display

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,15 +1,14 @@
-import { Component, Input /*ChangeDetectionStrategy*/ } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Project } from '../types/project';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.css'],
-  //changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 // @Input() allows a parent component (app.component) to update data in the child component (project.component)
 export class ProjectComponent {
   @Input()
-  projects: Project[];
+  project: Project[];
 }
